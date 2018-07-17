d64b4804016481a8604bfae97a33e756f577969d
Герман <stgoge@ya.ru>
2018-07-17 12:24:04 +0700

    +get and post

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ const { connectMongo } = require('./mongo/connect-mongo');
 const createAdmin = require('./js/create-admin');
 
 const startServer = async () => {
-  console.log('0');
   const server = await initServer();
-  console.log('1');
   const mongoose = await connectMongo();
-  console.log('2');
   await createAdmin();
   return { server, mongoose };
 };
--- test/records.js
+++ test/records.js
@@ -14,7 +14,7 @@ chai.use(chaiHttp);
 let token;
 let app;
 
-describe('Routes', () => {
+describe('Routes', async () => {
   const signup = '/signup';
   const signin = '/signin';
   const records = '/records';
@@ -28,8 +28,6 @@ describe('Routes', () => {
   };
   before(async () => {
     app = await startServer();
-    console.log(`server: ${app.server}`);
-    console.log(`mongoose: ${app.mongoose}`);
     const result = await chai
       .request(app.server)
       .post(signup)
@@ -38,13 +36,12 @@ describe('Routes', () => {
     token = result.body.token;
   });
   after('droping test db', async () => {
-    await app.mongoose.connection.dropDatabase(() => {
-      console.log('\n Test database dropped');
-    });
+    await app.mongoose.connection.dropDatabase();
+    console.log('\n Test database dropped');
     await app.mongoose.connection.close();
   });
-  describe('signup', () => {
-    it('should crete new user if username not found', async () => {
+  describe('signup', async () => {
+    it('should create new user if username not found', async () => {
       try {
         const result = await chai
           .request(app.server)
@@ -68,6 +65,94 @@ describe('Routes', () => {
         expect(error.status).to.equal(403);
         expect(error.response.text).to.equal('{"error":"Username is already in use"}');
       }
+    });
+  });
+  describe('signin', async () => {
+    it('should return error 400 if username and password empty', async () => {
+      const emptyUser = {};
+      try {
+        const result = await chai
+          .request(app.server)
+          .post(signin)
+          .send(emptyUser);
+      } catch (error) {
+        expect(error.status).to.be.equal(400);
+      }
+    });
+
+    it('should return 200 and token', async () => {
+      try {
+        const result = await chai
+          .request(app.server)
+          .post(signin)
+          .send(preSave);
+
+        expect(result.status).to.be.equal(200);
+        expect(result.body).not.to.be.empty;
+        expect(result.body).to.have.property('token');
+      } catch (error) {
+        throw new Error(error);
+      }
+    });
+  });
+  describe('records', async () => {
+    describe('post', async () => {
+      const testRecord = { record: 'test record', status: '1', priority: '2' };
+      const emptyRecord = { status: '1', priority: '2' };
+      it('without token. should return status 401', async () => {
+        try {
+          await chai.request(app.server).post(records).send(testRecord);
+        } catch (error) {
+          expect(error.status).to.equal(401);
+          expect(error.response.text).to.equal('Unauthorized');
+        }
+      });
+      it('with token and empty record. should return status 400', async () => {
+        try {
+          const result = await chai
+            .request(app.server)
+            .get(records)
+            .set('Authorization', token)
+            .send(emptyRecord);
+        } catch (error) {
+          expect(error.status).to.equal(401);
+        }
+      });
+      it('with token and correct record. should return status 200', async () => {
+        try {
+          const result = await chai
+            .request(app.server)
+            .get(records)
+            .set('Authorization', token)
+            .send(testRecord);
+          expect(result.status).to.be.equal(200);
+        } catch (error) {
+          throw new Error(error);
+        }
+      });
+    });
+    describe('get', async () => {
+      it('without token. should return status 401', async () => {
+        try {
+          await chai.request(app.server).get(records);
+        } catch (error) {
+          expect(error.status).to.equal(401);
+          expect(error.response.text).to.equal('Unauthorized');
+        }
+      });
+      it('with token. should return status 200', async () => {
+        try {
+          const result = await chai
+            .request(app.server)
+            .get(records)
+            .set('Authorization', token);
+          expect(result.status).to.equal(200);
+          expect(result.body).to.a('array');
+          expect(result.body.length).to.eql(0);
+        } catch (error) {
+          throw new Error(error);
+        }
+      });
     });
   });
 });
